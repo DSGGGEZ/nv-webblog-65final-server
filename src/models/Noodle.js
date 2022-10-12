6d0eb65fde086bcc4a0c7f1177329c2bd2f8c6fb
DSGGGEZ <88538783+DSGGGEZ@users.noreply.github.com>
2022-10-12 12:48:34 +0700

    ส่งข้อสอบครับ

---
+++ src/models/Noodle.js
@@ -0,0 +1,9 @@
+module.exports = (sequelize, DataTypes) => {
+    const Noodle = sequelize.define('Noodle', {
+        noodle_name: DataTypes.STRING,
+        noodles: DataTypes.STRING,
+        noodle_soup: DataTypes.STRING,
+        noodle_topping: DataTypes.STRING,
+    })
+    return Noodle
+}
--- src/route.js
+++ src/route.js
@@ -1,4 +1,7 @@
 const UserController = require('./controllers/UserController')
+const NoodleController = require('./controllers/NoodleController')
+const UserAuthenController = require('./controllers/UserAuthenController');
+const isAuthenController = require('./authen/isAuthenController')
 
 module.exports = (app) => {
     app.post('/user', UserController.create)
@@ -9,5 +12,19 @@ module.exports = (app) => {
     
     app.get('/user/:userId', UserController.show)
     
-    app.get('/users', UserController.index)
+    app.get('/users', isAuthenController, UserController.index)
+
+    //noodle
+    app.post('/noodle', NoodleController.create)
+    
+    app.put('/noodle/:noodleId', NoodleController.put)
+    
+    app.delete('/noodle/:noodleId', NoodleController.remove)
+    
+    app.get('/noodle/:noodleId', NoodleController.show)
+    
+    app.get('/noodles', NoodleController.index)
+
+    //login
+    app.post('/login', UserAuthenController.login)
 }
